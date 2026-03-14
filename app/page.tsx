9a8cb88c71638d136218cfa20e151176d13724ce
aaahanamehrotra <mehrotraaaahana@gmail.com>
2026-03-14 16:09:26 +0530

    minor ui fixes

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,16 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col justify-center p-8 md:p-24 bg-slate-50 dark:bg-slate-950">
+    <main className="flex min-h-screen flex-col justify-center p-8 bg-slate-50 dark:bg-slate-950">
       <div className="z-10 w-full max-w-5xl flex flex-col gap-8 font-mono text-sm">
         
-        <div className="text-center">
-          <h1 className="text-3xl font-bold tracking-tight sm:text-4xl mb-2">
+        {/* <div className="text-center"> */}
+          <h1 className="text-3xl font-bold tracking-tight sm:text-4xl mb-2 te">
             Live Drone Tracker
           </h1>
           <p className="text-slate-500 dark:text-slate-400">
           </p>
-        </div>
+        {/* </div> */}
 
         {!isConfigured ? (
           <div className="w-full bg-white dark:bg-slate-900 p-4 sm:p-6 rounded-xl shadow-md border border-slate-200 dark:border-slate-800">
